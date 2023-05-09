07a23d3ce8d62d4169181a249e47ef1d278ac61f
AndersonBLP <andersonblorenzopena@gmail.com>
2023-05-09 18:05:07 -0400

    finalizando encriptado

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ var textarea = document.querySelector(".textarea");
 var resultado = document.querySelector(".resultado")
 var copiar = document.querySelector(".btn-copiar")
 
+
 botonEncriptar.onclick = encriptar;
 botonDesencriptar.onclick = desencriptar;
+
 
 function ocultarDiv(){
     muneco.classList.add("ocultar")
@@ -36,11 +38,65 @@ function recuperarTexto(){
 }
 
 
-function encriptarTexto(){
-  
+
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal= "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
 }
 
-function desencriptarTexto(){
-    
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
 }
-    
+
+
